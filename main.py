cb7ecade204219bcca93f04a1046b0d46eb603e6
anup2230 <anup2230@gmail.com>
2023-11-01 02:24:51 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2, numpy as np
+import dlib
+
+cap = cv2.VideoCapture(0)
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+while True:
+    _, frame = cap.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray)
+
+    for face in faces:
+        x, y = face.left(), face.top()
+        x1, y1 = face.right(), face.bottom()
+        cv2.rectangle(frame, (x,y), (x1,y1), (0,255,0), 2)
+        landmarks = predictor(gray, face)
+        x = landmarks.part(36).x
+        y = landmarks.part(36).y
+        cv2.circle(frame, (x,y), 3, (255,0,0), 2)
+    
+    cv2.imshow("Frame", frame)
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+    
+cap.release()
+cv2.destroyAllWindows() 
